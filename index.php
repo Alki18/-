a636aa0f5fe53fa7aad0741f958ccc240b6b1ddd
Alki18 <apetrosan881@gmail.com>
2025-09-12 12:34:04 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p> ф.и.о</p> <input type="text" name "fio">
+    <p>адрес</p> <input type="text" name "adress">
+    <p>почта</p> <input type="text" name "email">
+    <p>пароль</p> <input type="text" name "password">
+
+</body>
+</html>
